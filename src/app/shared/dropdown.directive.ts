ce52fc122a5f256563fe8ab893acf71c6e4ccf4a
BisanGhoul <bbisann@gmail.com>
2023-09-22 18:35:29 +0300

    close dropdown from anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 
 @Directive({
@@ -8,8 +8,18 @@ export class DropDown{
 
     @HostBinding('class.open') open: boolean = false;
 
-    @HostListener('click') openMenu(){
-        this.open = !this.open;
-        
-    }
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.open = this.elRef.nativeElement.contains(event.target) ? !this.open : false;
+      }
+      constructor(private elRef: ElementRef) {}
 }
+
+/**
+ * 
+ * this.open = this.elRef.nativeElement.contains(event.target) ? !this.open : false;
+
+    Inside the toggleOpen method, this line checks whether the clicked element (event.target) is contained within the host element (this.elRef.nativeElement).
+    If the clicked element is inside the host element, it means the click occurred within the dropdown, and the open property is toggled (!this.open) to open or close the dropdown accordingly.
+    If the clicked element is outside the host element (i.e., not contained within it), the open property is set to false, closing the dropdown.
+ * 
+ */
