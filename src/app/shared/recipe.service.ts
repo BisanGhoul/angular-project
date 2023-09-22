fe83d2efd50c9435a99481a3d73a9a95f6c4938e
BisanGhoul <bbisann@gmail.com>
2023-09-22 21:25:14 +0300

    add ingredients to recipes

--- src/app/shared/recipe.service.ts
+++ src/app/shared/recipe.service.ts
@@ -1,14 +1,19 @@
 import { EventEmitter } from "@angular/core";
 import { Recipe } from "../recipes/recipe.model";
+import { Ingredient } from "./ingredient.model";
 
 export class RecipeService{
  
     selectedRecipe = new EventEmitter<Recipe>();
     private recipes : Recipe[] = [
         new Recipe("Maqloubeh", "Palestinian cuisine",
-    "https://jonoandjules.files.wordpress.com/2019/06/maqloubeh-rice-with-aubergines-peppers.jpg?w=584"),
+    "https://jonoandjules.files.wordpress.com/2019/06/maqloubeh-rice-with-aubergines-peppers.jpg?w=584",
+    [new Ingredient("Rice",2)]),
+
     new Recipe("Maqloubaah 2", "Palestinian cuisine",
-    "https://jonoandjules.files.wordpress.com/2019/06/maqloubeh-rice-with-aubergines-peppers.jpg?w=584")
+    "https://jonoandjules.files.wordpress.com/2019/06/maqloubeh-rice-with-aubergines-peppers.jpg?w=584",
+    [new Ingredient("Rice",2), new Ingredient("Tomatoe",3)])
+
     ];
 
     public getRecipes(){
