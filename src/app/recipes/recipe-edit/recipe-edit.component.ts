1280dfac019c8f87612014cefad94b2390a7ce63
BisanGhoul <bbisann@gmail.com>
2023-11-11 21:40:43 +0200

    Adding Ingredient Controls to a Form Array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, NgForm } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from 'src/app/shared/recipe.service';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -29,14 +30,19 @@ export class RecipeEditComponent implements OnInit {
     )
   }
 
+  get controls() { // a getter!
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   onSubmit() {
-    console.log(this.recipeForm);
+    console.log(this.recipeService.getRecipeById(this.id));
   }
 
   private initForm(){
     let recipeName = '';
     let recipeImgPath = '';
     let recipeDesc = '';
+    let recipeIngredients = new FormArray([]);
 
       if(this.isEditMode){
         const recipe: Recipe = this.recipeService.getRecipeById(this.id);
@@ -44,12 +50,23 @@ export class RecipeEditComponent implements OnInit {
         recipeName = recipe.name;
         recipeImgPath = recipe.imgPath;
         recipeDesc = recipe.description;
+        if(recipe['ingredients']){
+          for(let ing of recipe.ingredients){
+            recipeIngredients.push(
+              new FormGroup({
+                'name': new FormControl(ing.name),
+                'amount': new FormControl(ing.amount)
+              })
+            )
+          }
+        }
       }
 
     this.recipeForm = new FormGroup({
       'name:': new FormControl(recipeName),//if we are in editmode will have the name of the recipe, if not will be empty
       'imagePath': new FormControl(recipeImgPath),
-      'description': new FormControl(recipeDesc)
+      'description': new FormControl(recipeDesc),
+      'ingredients': recipeIngredients
     })
   }
 
