a60c5de4e9fce89449a77c33ddae5952d5615906
BisanGhoul <bbisann@gmail.com>
2023-12-31 19:23:07 +0200

    Attaching the Token with an Interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +19,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from './shared/recipe.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,10 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
   ],
   providers: [
     ShoppingListService,
-    RecipeService
+    RecipeService,
+    {provide: HTTP_INTERCEPTORS,
+       useClass: AuthInterceptorService,
+      multi: true}
   ],
   bootstrap: [AppComponent]
 })
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,82 +1,71 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { RecipeService } from "./recipe.service";
-import { Recipe } from "../recipes/recipe.model";
-import { AuthService } from "../auth/auth.service";
-import { exhaustMap, map, take, tap } from "rxjs";
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from './recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
+import { exhaustMap, map, take, tap } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
-export class DataStorageService{
-    constructor(private http: HttpClient,
-                private recipesService: RecipeService,
-                private authSer: AuthService){}
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(
+    private http: HttpClient,
+    private recipesService: RecipeService,
+    private authSer: AuthService
+  ) {}
 
-    //inject recipe service or tale recipes as an arg and save them            
-    storeRecipe(){
+  //inject recipe service or tale recipes as an arg and save them
+  storeRecipe() {
+    const recipes = this.recipesService.getRecipes();
+    this.http
+      .put(
+        'https://ng-course-recipe-book-191eb-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((res) => {
+        console.log(res); //sub where u call it (header comp) if u want to add a spinner for example
+      }); // request will be sent when we subsrcibe
+  }
 
-        const recipes = this.recipesService.getRecipes();
-        this.http.put('https://ng-course-recipe-book-191eb-default-rtdb.firebaseio.com/recipes.json',
-        recipes).subscribe(
-            res => {
-                console.log(res);//sub where u call it (header comp) if u want to add a spinner for example
-            }
-        )// request will be sent when we subsrcibe 
-    }
+  // fetchRecipes(){
+  //     return this.authSer.user.pipe(
+  //         take(1),
+  //         exhaustMap(user =>{
+  //             return this.http.get<Recipe[]>('https://ng-course-recipe-book-191eb-default-rtdb.firebaseio.com/recipes.json',
+  //             {
+  //                 params: new HttpParams().set('auth', user.token)
+  //             }
+  //             );
+  //         }),
+  //         map(recipes => {
+  //             return recipes.map(recipe => {
+  //                 return{
+  //                     ...recipe,
+  //                     ingredients: recipe.ingredients? recipe.ingredients : []
+  //                 }
+  //             })
+  //         }),
+  //         tap(recipes =>{
+  //             this.recipesService.setRecipes(recipes)
+  //         }))//or unsub right after
 
-    // fetchRecipes(){
-    //     return this.authSer.user.pipe(
-    //         take(1),
-    //         exhaustMap(user =>{
-    //             return this.http.get<Recipe[]>('https://ng-course-recipe-book-191eb-default-rtdb.firebaseio.com/recipes.json',
-    //             {
-    //                 params: new HttpParams().set('auth', user.token)
-    //             }
-    //             );
-    //         }),
-    //         map(recipes => {
-    //             return recipes.map(recipe => {
-    //                 return{
-    //                     ...recipe,
-    //                     ingredients: recipe.ingredients? recipe.ingredients : []
-    //                 }
-    //             })
-    //         }),
-    //         tap(recipes =>{
-    //             this.recipesService.setRecipes(recipes)
-    //         }))//or unsub right after 
-
-    // }
-    fetchRecipes() {
-        return this.authSer.user.pipe(
-          take(1),
-          exhaustMap(user => {
-            return this.http.get<Recipe[]>(
-                'https://ng-course-recipe-book-191eb-default-rtdb.firebaseio.com/recipes.json',
-              {
-                params: new HttpParams().set('auth', user.token)
-              }
-            );
-          }),
-          map(recipes => {
-            return recipes.map(recipe => {
-              return {
-                ...recipe,
-                ingredients: recipe.ingredients ? recipe.ingredients : []
-              };
-            });
-          }),
-          tap(recipes => {
-            this.recipesService.setRecipes(recipes);
-          })
-        );
-      }
-    
+  // }
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-191eb-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipesService.setRecipes(recipes);
+        })
+      );
+  }
 }
-
-
-
-
-
-
-
-
